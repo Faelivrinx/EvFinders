a5bd9d84b5558da9acf9087cd69d08293971c6d0
Faelivrin <jurasz.do@gmail.com>
2017-06-22 16:33:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dominik/evfinders/application/EvApplication.java
@@ -0,0 +1,10 @@
+package com.example.dominik.evfinders.application;
+
+import android.app.Application;
+
+/**
+ * Created by Dominik on 22.06.2017.
+ */
+
+public class EvApplication extends Application {
+}
